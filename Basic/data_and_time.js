f1de67ee53bdb228a34241fb8f904be3bebfbce0
Rahul Vishwakarma <93070303+vishrahul2002@users.noreply.github.com>
2023-10-05 18:27:59 +0000

    different methos of date

--- Basic/data_and_time.js
+++ Basic/data_and_time.js
@@ -1,2 +1,7 @@
 const myData = new Date();
 console.log(myData);
+
+console.log(myData.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
