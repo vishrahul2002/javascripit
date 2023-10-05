4c54eef676c14b84902c3352338b11c05b9ecd2f
Rahul Vishwakarma <93070303+vishrahul2002@users.noreply.github.com>
2023-10-05 18:32:28 +0000

    Updating the vairable and and type

--- Basic/data_and_time.js
+++ Basic/data_and_time.js
@@ -1,7 +1,7 @@
-const myData = new Date();
-console.log(myData);
+let myDate = new Date();
+console.log(myDate);
 
-console.log(myData.toString());
+console.log(myDate.toString());
 console.log(myDate.toDateString());
 console.log(myDate.toLocaleString());
 console.log(typeof myDate);
