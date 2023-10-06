16d09ad5441e4f015800a8c60a796ad459be2731
Rahul Vishwakarma <93070303+vishrahul2002@users.noreply.github.com>
2023-10-06 17:32:53 +0000

    learning array and it's method

---
+++ 02_Basic/01_arrays.js
@@ -0,0 +1,20 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5];
+
+const Heros = ["Ironman", "Loki", "Thor"];
+
+const myArr2 = new Array(1, 2, 3, 4, 5);
+
+
+// +++++++++ Array Methods +++++++++
+// myArr.push(6); // Add's element at the last in Array.
+// myArr.push(7);
+// myArr.pop() // remove the last element from Array
+
+
+
+
+
+
+console.log(myArr)
