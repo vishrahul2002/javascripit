828eb0ab204136f8802b9d6bdf3ba199dcbda67b
Rahul Vishwakarma <93070303+vishrahul2002@users.noreply.github.com>
2023-10-17 06:07:24 +0000

    Singleton and literal concept of an object

---
+++ 02_Basic/03_objects.js
@@ -0,0 +1,10 @@
+// Singleton
+
+// literal is single vairable or const used to create an object in a single variable.
+// eg : 
+        // let dog1 = {
+        //     name: german, 
+        //     color: black,
+        //     age: 5
+        // }
+
