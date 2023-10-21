a3e0d30bf92a06586fab3eb09e85a37634b8631c
Rahul Vishwakarma <93070303+vishrahul2002@users.noreply.github.com>
2023-10-21 05:59:45 +0000

    Object `assign` method & `spread operator` method and  few experiments on objects

--- 02_Basic/04_objects.js
+++ 02_Basic/04_objects.js
@@ -20,9 +20,35 @@ const regularUser = {
 }
 
 // accessing the nested object
-console.log(regularUser.fullName.userFullName.firstName); // using `.` to access nested object
+// console.log(regularUser.fullName.userFullName.firstName); // using `.` to access nested object
 // 
 // optional chaining
-console.log(regularUser.fullName?.userFullName.firstName); // will learn in further concepts.
+// console.log(regularUser.fullName?.userFullName.firstName); // will learn in further concepts.
 
 // how to merge or combine objects
+
+const obj1 = {1: "a", 2: "b"};
+const obj2 = {3: "c", 4: "d"};
+const obj3 = {5: "e", 6: "e"};
+
+// merging
+const obj4 = {obj1, obj2, obj3};
+console.log(obj4); // it will return the objects inside the object 
+// using an object method to make a copy of an array into another array
+
+const obj5 = Object.assign({}, obj1, obj2, obj3) // Copy the values of all of the enumerable own properties from one or more source objects to a target object. Returns the target object.
+// console.log(obj5);
+
+// experiment
+
+const egobj = Object.assign(obj1, obj2, obj3) // here the obj1 will be the target and all the other object will be source so either we can use egobj or obj1 for reference of copy
+// console.log(egobj);
+console.log(obj1);
+console.log(egobj === obj1);
+
+
+// Sperate Operator
+// best and easy way to add objects
+
+const combineobj = {...obj1, ...obj2};
+console.log(combineobj);
