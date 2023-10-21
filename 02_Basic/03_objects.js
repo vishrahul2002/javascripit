c59e4f39cde52d40660968e000b1ba0d439589a8
Rahul Vishwakarma <93070303+vishrahul2002@users.noreply.github.com>
2023-10-21 03:38:19 +0000

    Learned Symbol datatype and how to use and access in the object

--- 02_Basic/03_objects.js
+++ 02_Basic/03_objects.js
@@ -2,9 +2,31 @@
 
 // literal is single vairable or const used to create an object in a single variable.
 // eg : 
-        // let dog1 = {
-        //     name: german, 
-        //     color: black,
-        //     age: 5
-        // }
 
+// Creating a Symbol 
+
+const mySum = Symbol("key1");
+
+        let JsUser = {
+            name: "Rahul",
+            "full name" : "Rahul Vishwakarma",
+            // using a symbol
+            [mySum] : "myKey1",
+            age: 5,
+            location: "Mumbai",
+            email: "rahul@gmail.com",
+            isLoggedIn: false,
+            lastLoggedinDate : ["Monday", "Satruday"]
+        }
+
+// accessing the object 
+
+// console.log(JsUser.email); // one way to access the object
+// console.log(JsUser["email"]); // bestway to acces the object
+
+// console.log(JsUser["full name"]); // to access the string used as a key in the object
+
+
+// using a Symbol in an object and accesing them 
+console.log(JsUser[mySum]);
+console.log(typeof JsUser[mySum]);
