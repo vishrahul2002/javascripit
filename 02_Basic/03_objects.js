cbcb9a7555b370c2c9cd252ae6e07d08589010d6
Rahul Vishwakarma <93070303+vishrahul2002@users.noreply.github.com>
2023-10-21 04:06:23 +0000

    Using function with object, and bit about `this` keyword.

--- 02_Basic/03_objects.js
+++ 02_Basic/03_objects.js
@@ -22,25 +22,37 @@ let JsUser = {
 // accessing the object 
 
 // console.log(JsUser.email); // one way to access the object
-// console.log(JsUser["email"]); // bestway to acces the object
+// console.log(JsUser["email"]); // bestway to access Symbol in object
 
 // console.log(JsUser["full name"]); // to access the string used as a key in the object
 
 
 // using a Symbol in an object and accesing them 
-console.log(JsUser[mySum]);
+// console.log(JsUser[mySum]);
 // console.log(typeof JsUser[mySum]);
 
 // Learning to manulipate the object actual value
 
 JsUser.name = "Vish" // the value has been changed to vish
 
-console.log(JsUser["name"]);
+// console.log(JsUser["name"]);
 
 
 // Freasing the object so that no one can modifiy the value of an actual object
 
-Object.freeze(JsUser) // from here onward JSUser can't be modifiyed
+// Object.freeze(JsUser) // from here onward JSUser can't be modifiyed
 
 JsUser.name = "Rahul" // here the value of name will not change in the object
-console.log(JsUser);
+// console.log(JsUser);
+
+// Using a function in object
+JsUser.greeting = function () {
+    console.log("Hello JSUser");
+}
+
+JsUser.greetingTwo = function () {
+    console.log(`Hello JSUser, Your name is ${this.name}`); // here [this] is used to access the object elements
+}
+
+console.log(JsUser.greeting()); 
+console.log(JsUser.greetingTwo()); 
