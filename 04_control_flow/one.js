a5cac5bfe55768789b54a6ddb30027c5e4805f9d
Rahul Vishwakarma <93070303+vishrahul2002@users.noreply.github.com>
2023-11-09 08:16:58 +0000

    Learning the control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,27 @@
+// Learning about control flow in javascript
+
+// JavaScript operator 
+// <, >, <=, >=, !=, ==, ===
+
+// eg:
+// if (2 != "2") {
+//     console.log("Executed");
+// }
+
+// Implicite Scope : Do not use this type of syntax to write a code
+// const balance = 1000;
+
+// if (balance > 500) console.log("Yes"), // we asume that the log is in the scope and we don't use `{}`
+// console.log("the balance is more than 500"); // in order to use more than one line we can use `,`
+
+
+// if and else nesting
+// if ( balance < 500 ) {
+//     console.log("The balance is less than 500");
+// } else if ( balance < 750 ) {
+//     console.log("The balance is less than 750");
+// } else if ( balance < 950 ) {
+//     console.log("The balance is less than 950");
+// } else {
+//     console.log("The balance is less than 1500");
+// }
