f7392a15f0792399bf1adeeeb4aa183088266242
Rahul Vishwakarma <93070303+vishrahul2002@users.noreply.github.com>
2023-12-04 07:31:37 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,32 @@
+// Syntax for switch statement
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+const month = 'march';
+
+switch ('march') {
+    case 'jan':
+        console.log('1st month');
+        break
+    case 'feb':
+        console.log('2nd month');
+        break
+    case 'march':
+        console.log('3rd month');
+        break
+    case 'april':
+        console.log('4th month');
+        break
+    default:
+        console.log('default value executed');
+        break
+}
