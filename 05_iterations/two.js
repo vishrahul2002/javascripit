cbb7368f7d3cf7e905c1e220305f76b27260394b
Rahul Vishwakarma <93070303+vishrahul2002@users.noreply.github.com>
2023-12-12 08:37:39 +0000

    learning about do_while loop

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -9,10 +9,18 @@ let index = 0;
 
 // on array
 
-let myArray = ["Batman", "Superman", "Flash", "Wonder_Women", "Joker"];
-let arrCount = 0
+// let myArray = ["Batman", "Superman", "Flash", "Wonder_Women", "Joker"];
+// let arrCount = 0
 
-while (arrCount < myArray.length) {
-    console.log(`Value is ${myArray[arrCount]}`);
-    arrCount += 1;
-}
+// while (arrCount < myArray.length) {
+//     console.log(`Value is ${myArray[arrCount]}`);
+//     arrCount += 1;
+// }
+
+// Do while loop
+
+let score = 11;
+do {
+    console.log(`Score is ${score}`); // here it will run atleast one time if the condition is false
+    score++
+} while (score <= 10);
