8c8ef7e4358e7e41bd5f32c993995c035404047a
Rahul Vishwakarma <93070303+vishrahul2002@users.noreply.github.com>
2023-12-12 08:18:02 +0000

    learning while loop and looping on array

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+// while loop
+
+let index = 0;
+
+// while (index <= 10) {
+//     console.log(`Value is ${index}`);
+//     index += 2;
+// }
+
+// on array
+
+let myArray = ["Batman", "Superman", "Flash", "Wonder_Women", "Joker"];
+let arrCount = 0
+
+while (arrCount < myArray.length) {
+    console.log(`Value is ${myArray[arrCount]}`);
+    arrCount += 1;
+}
