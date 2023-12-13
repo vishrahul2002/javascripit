85bf2ebc6bd7e73898b0738d83eacd6197025bff
Rahul Vishwakarma <93070303+vishrahul2002@users.noreply.github.com>
2023-12-13 09:18:25 +0000

    learning to loop through array object using for each loop

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -13,8 +13,37 @@ const coding = ['java', 'python', 'c++', 'c#', 'javascript'];
 
 // using another function 
 
-const printMe = (item) => {
-    console.log(item);
-}
+// const printMe = (item) => {
+//     console.log(item);
+// }
 
-coding.forEach(printMe) // here we just have to give the reference of the fucntion 
+// coding.forEach(printMe) // here we just have to give the reference of the fucntion 
+
+// using more function of the for Each loop
+
+// coding.forEach( (val, index, arr) => {
+//     console.log(index, val, arr);
+// })
+
+// object inside the array 
+
+const program = [
+    {
+        languageName : 'Java',
+        languageFileName : 'java'
+    },
+    {
+        languageName : 'Javascript',
+        languageFileName : 'js'
+    },
+    {
+        languageName : 'python',
+        languageFileName : 'py'
+    }
+]
+
+// for looping throw database result
+
+program.forEach( (val) => {
+    console.log(val.languageFileName);
+})
