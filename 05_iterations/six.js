2f6a0ebc91f9e45f5c5d63bb258ef9b42fdebaef
Rahul Vishwakarma <93070303+vishrahul2002@users.noreply.github.com>
2023-12-15 03:05:52 +0000

    learning about for each

---
+++ 05_iterations/six.js
@@ -0,0 +1,8 @@
+const coding = ['java', 'python', 'c++', 'c#', 'javascript'];
+
+const values = coding.forEach ( (item) => {
+    // console.log(item);
+    return item
+})
+
+console.log(values); // for each does not return the value for that we can use map or somthing else
