7c81ba8be7a2850f7d0f616c5f60cfa1a5af1cc5
Rahul Vishwakarma <93070303+vishrahul2002@users.noreply.github.com>
2023-12-16 06:35:01 +0000

    learning more about map function

---
+++ 05_iterations/seven.js
@@ -0,0 +1,17 @@
+// Maps
+
+const myNums = [1,2,3,4,5,6,7,8,9,10];
+
+// using map function
+
+// const newNums = myNums.map( (num) => {return num + 10 })
+
+
+
+// using for each loop
+
+// myNums.forEach ( (num, index, arr) => {
+//     return arr[index] = num + 10
+// })
+
+// console.log(myNums);
