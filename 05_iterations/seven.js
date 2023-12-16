82916225b4c2fc213ca9d59d6d1baf5232e61f77
Rahul Vishwakarma <93070303+vishrahul2002@users.noreply.github.com>
2023-12-16 06:41:04 +0000

    Learning about `Optional chaining`

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -15,3 +15,14 @@ const myNums = [1,2,3,4,5,6,7,8,9,10];
 // })
 
 // console.log(myNums);
+
+// chaining in js
+
+let num = [1,2,3,4,5,6,7,8,9,10];
+
+let newNum = num // using mulitple operation's is chaining
+.map( (num) => num * 10)
+.map( (num) => num + 5) // here it will take the above map function result and perform the operation
+.filter( (num) => num >= 50) // here it will take the above result and perform the task
+
+console.log(newNum);
