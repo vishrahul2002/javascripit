8ba760e35c4f629943ce02fff582322222278135
Rahul Vishwakarma <93070303+vishrahul2002@users.noreply.github.com>
2024-01-16 05:08:15 +0000

    Learning about the `Constructor function`.

--- 10_OOP_and_Class/oop.js
+++ 10_OOP_and_Class/oop.js
@@ -13,5 +13,24 @@ const user = {
 
 // console.log(user["signedIn"])
 // console.log(user.username)
+// console.log(user.getUserDetails());
 
-console.log(user.getUserDetails());
+// Constructor function
+// const promise = new Promise()
+// const date = new Date()
+// here the new and the Promise is the constructor function
+
+function User(username, loginCount, isLoggedIn) {
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    return this
+}
+
+
+const userOne = new User('rahul', 10, 234)
+const userTwo = new User('LearnJS', 4, 1234)
+// console.log(userOne);
+console.log(userTwo.constructor); 
+// there is also an another function which is called `instanceOf` learn more on google
