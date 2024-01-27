51f3f92bcba73f0cd8740dca098575e85ab2be2e
Rahul Vishwakarma <93070303+vishrahul2002@users.noreply.github.com>
2024-01-27 06:23:08 +0000

    Behind the Scene this is how classes work

--- 10_OOP_and_Class/myClasses.js
+++ 10_OOP_and_Class/myClasses.js
@@ -1,24 +1,49 @@
 // Classes are used from ES6
 
 // creating a class
-class User {
-    //NOTE:  when enver we use new key word the constructor will be called automatically 
-    constructor(username, email, password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
-    // Method
-    encryptPassword() {
-        return `${this.password}abc`
-    }
+// class User {
+//     //NOTE:  when enver we use new key word the constructor will be called automatically 
+//     constructor(username, email, password) {
+//         this.username = username;
+//         this.email = email;
+//         this.password = password;
+//     }
+//     // Method
+//     encryptPassword() {
+//         return `${this.password}abc`
+//     }
 
-    changeUsername() {
-        return `${this.username.toUpperCase()}`
-    }
+//     changeUsername() {
+//         return `${this.username.toUpperCase()}`
+//     }
+// }
+
+// const user1 = new User('rahul', 'rahul.fb.com', '123')
+
+// console.log(user1.encryptPassword());
+// console.log(user1.changeUsername());
+
+// behind the scene
+
+// before the class we introduce the code was written this way
+
+function User (username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
 }
 
-const user1 = new User('rahul', 'rahul.fb.com', '123')
+// Because the function is also an Object we can use the prototype with the function 
 
-console.log(user1.encryptPassword());
-console.log(user1.changeUsername());
+User.prototype.encryptPassword = function () {
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function () {
+    return `${this.username.toUpperCase()}`
+}
+
+const vish = new User('vish', 'vish@google.com', '111')
+
+console.log(vish.changeUsername());
+console.log(vish.encryptPassword());
+
